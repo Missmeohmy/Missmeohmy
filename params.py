1fe059ee0e9f03be062cb64bb31a863b1c143544
Missmeohmy <109921208+Missmeohmy@users.noreply.github.com>
2023-05-09 12:44:43 -0400

    Add files via upload

---
+++ params.py
@@ -0,0 +1,11 @@
+#This code will show how to use perameters when defining a function#
+def greet (lang):
+    if lang == 'es':
+        print('Hola')
+    elif lang == 'fr':
+        print('Bonjour')
+    else:
+        print('Hello')
+greet('en')
+greet('es')
+greet('fr')
