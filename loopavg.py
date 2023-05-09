d936049c937dc551264bff5c434c4c6a02063329
Missmeohmy <109921208+Missmeohmy@users.noreply.github.com>
2023-05-09 15:55:36 -0400

    Update loopavg.py

--- loopavg.py
+++ loopavg.py
@@ -1,7 +1,7 @@
 #This program will add the show the average of the items within a loop
 count = 0
 sum = 0
-print("Start", count)
+print("Start", count, sum)
 for num in [1, 63, 6, 89, 22, 45, 96, 3, 6, 45, 77, 100, 352, 6251, 987, 663]:
     count = count + 1
     sum = sum + num
