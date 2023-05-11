79b69281d238f554aab740864736e76e4f5cf73e
Missmeohmy <109921208+Missmeohmy@users.noreply.github.com>
2023-05-11 10:05:12 -0400

    Add files via upload

---
+++ lcount.py
@@ -0,0 +1,9 @@
+#This is code is another example from Coursera of looping and counting.
+
+word = 'banana'
+count = 0
+for letter in word:
+    if letter == 'a' :
+        count = count + 1
+    print(count)
+    
---
+++ slicingstr.py
@@ -0,0 +1,6 @@
+#This program from Coursera is an example of slicing strings for specific information
+
+s = 'Monty Python'
+print(s[0:4])
+print(s[6:7])
+print(s[6:20])
