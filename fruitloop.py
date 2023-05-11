54884aa5b5b47a0de616f0ea91febd0b41fe34e3
Missmeohmy <109921208+Missmeohmy@users.noreply.github.com>
2023-05-11 09:52:42 -0400

    Add files via upload

---
+++ fruitloop.py
@@ -0,0 +1,7 @@
+#This code shows an example of looping through strings/code obtained from coursera 
+fruit = 'banana'
+index = 0
+while index < len(fruit):
+    letter = fruit[index]
+    print(index, letter)
+    index = index + 1
