d9b45f3d73eb2e3eaade73f556c8273c45b4638f
Missmeohmy <109921208+Missmeohmy@users.noreply.github.com>
2023-05-15 15:01:25 -0400

    Add files via upload

---
+++ overlap.py
@@ -0,0 +1,8 @@
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+c = a + b
+lst = []
+for num in c:
+    if num not in lst:
+        lst.append(num)
+print(lst)
