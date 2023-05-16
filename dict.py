466f0fd1711b5cf9366b4f856259027aa3af15bc
Missmeohmy <109921208+Missmeohmy@users.noreply.github.com>
2023-05-16 11:21:40 -0400

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,10 @@
+#This program will create a dictionary with the conditions that if a name is in the dictionary add 1 to the count. If the name is not in the dictionary it will add the name to the dictionary and add one to the count for it.
+
+counts = dict()
+names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
+for name in names:
+    if name not in counts:
+        counts[name] = 1
+    else:
+        counts[name] =counts[name]+1
+prints(counts)
