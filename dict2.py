3ba546bf3dd5932dece5a343154de8cde3b71ac4
Missmeohmy <109921208+Missmeohmy@users.noreply.github.com>
2023-05-17 12:38:29 -0400

    Add files via upload

---
+++ dict2.py
@@ -0,0 +1,7 @@
+#This program is simplified counting with get()
+
+counts = dict()
+names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
+for name in names:
+    counts[name] = counts.get(name, 0) +1
+print(counts)
