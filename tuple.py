055008bd7d213edb24a8ede10773408ca70f65b6
Missmeohmy <109921208+Missmeohmy@users.noreply.github.com>
2023-05-18 09:52:50 -0400

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,17 @@
+#This code from coursera is an example of using a reverse tuple to count values, keys for sorting. 
+
+fhand =  ('romeo.txt')
+counts =  dict()
+for line in fhand:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+        
+lst = list()
+for key, val in counts.items():
+    newtup = (val, key)
+    lst.append(newtup)
+
+lst = sorted(lst, reverse=True)
+for val, key in lst[:10]:
+    print(key, val)
