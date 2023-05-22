a4bb9064e086aa234af8d08eb8c9ca032a9018f8
Missmeohmy <109921208+Missmeohmy@users.noreply.github.com>
2023-05-22 08:31:15 -0400

    Add files via upload

---
+++ ggame1.py
@@ -0,0 +1,36 @@
+#This program will generate a random number between 1 and 9. It will ask the user to guess the number then tell the user if their guess was too high, low or perfect. 
+
+import random
+
+def num():
+    count = 0
+    a = random.randint(1,9)
+    while a != 0:
+        guess = input("What is your guess? Type '0' to quit: ")
+        guess = int(guess)
+        count += 1
+        if guess == 0:
+            print("Sayonara!")
+            quit()
+        elif guess == a:
+            print("Well look at the big brain on", name, "!")
+            print("It took you", count, "tries!")
+            print("Do you want to play again?")
+            replay = input("Type 'Yes'or 'Exit': ").lower()
+            if replay == 'yes':
+                num()  
+            else:
+                print("Sayonara!")
+                quit()
+        
+        elif guess <= a:
+            print("Too low amigo! Try another!")
+        
+        else:
+            print("Way too high compadre! Guess again!")
+                
+      
+        
+name = input("Welcome to the guessing game! Please enter your name: ") 
+print("Try to guess what number is selected between 1-9.")
+num()
